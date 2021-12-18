e2db1b218d597b45f3a5472b8e6197c5b74e4aaa
Ian Lee <ianl923@icloud.com>
2021-12-18 14:13:13 -0500

    .

--- 0.ReversePolishStack.py
+++ 0.ReversePolishStack.py
@@ -1,28 +1,26 @@
 def evalRPN(tokens) -> int:
-        # go through each element
-        # keep last two tokens in stack, pop
-        # finish until we reach end of list
-        to_eval = []
+    # go through each element
+    # keep last two tokens in stack, pop
+    # finish until we reach end of list
+    
+    operations = {'+': lambda a, b: a+b,
+                    '-': lambda a, b: a-b,
+                    '*': lambda a, b: a*b,
+                    '/': lambda a, b: int(a/b)}
+    stack = []
+    for token in tokens:
+        if token in '+-*/':
+            # Get operation function
+            op = operations[token]
+            if len(stack) >= 2:
+                last = stack.pop()
+                second_last = stack.pop()
+                stack.append(op(second_last, last))
+            else:
+                print('Invalid operation')
+        else:
+            # Must be a number (postive or negative)
+            stack.append(int(token))
+    return stack[-1]
         
-        for token in tokens:
-            # Token is a number
-            try:
-                to_eval.append(int(token)) # Add to stack
-            except:
-                # Token is an operand
-                if len(to_eval) >= 2:
-                    last = to_eval.pop()
-                    second_last = to_eval.pop()
-                    # Perform operation
-                    if token == '+':
-                        result = second_last + last
-                    elif token == '-':
-                        result = second_last - last
-                    elif token == '*':
-                        result = second_last * last
-                    elif token == '/':
-                        result = int(second_last/last)
-                    # Add back to stack
-                    print(result)
-                    to_eval.append(result)
-        return to_eval.pop()
+    
